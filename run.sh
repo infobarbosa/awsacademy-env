de13b3be3fb483e99bb69ca335eb591313861474
infobarbosa <infobarbosa@yahoo.com.br>
2024-08-03 20:01:56 -0300

    chore: Remove unused AWS CloudFormation command from run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-#aws cloudformation create-stack --stack-name myteststack --template-body file://sampletemplate.json --parameters ParameterKey=KeyPairName,ParameterValue=TestKey ParameterKey=SubnetIDs,ParameterValue=SubnetID1\\,SubnetID2
-aws cloudformation create-stack --stack-name meustack --template-body file://template1.yaml
